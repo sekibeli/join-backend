25fe635a5ac48f13edaead5883832166c14b12b5
Julia Georgiew <java2011@me.com>
2023-10-17 16:46:12 +0200

    settings.py

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-*(1zxw9%^6!f8b+0)+=@3-+@8grpj8#f-)30+d!3&%@+&l&1*g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'joinbackend.pythonanywhere.com/',
+    '127.00.1',
+    'localhost',
+]
 CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
