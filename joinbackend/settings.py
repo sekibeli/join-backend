2bdfc3c0bf4f05454c00bb2fdb2ec89a6c47d88c
Julia Georgiew <java2011@me.com>
2023-10-18 17:18:13 +0200

    static ordner

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = "/home/joinbackend/join-backend/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
