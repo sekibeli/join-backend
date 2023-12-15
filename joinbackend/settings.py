4713fae167ca70d529efb56c5c015348e7006e95
Julia Georgiew <java2011@me.com>
2023-12-15 18:21:24 +0100

    credentials

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -39,6 +39,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 #     ]
 CORS_ALLOW_METHODS = ["DELETE", "GET", "OPTIONS", "PATCH", "POST", "PUT"]
 # Application definition
+CORS_ALLOW_CREDENTIALS = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
