563e7ed5eef51bd17a1a94595c6ba35d2be9b687
Julia Georgiew <java2011@me.com>
2023-12-16 18:48:52 +0100

    allowed hosts

--- joinbackend/settings.py
+++ joinbackend/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = [
     'julia-georgiew.developerakademie.net',
     'myjoin.julia-georgiew.de'
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:4200',
     'http://localhost:4200',
@@ -57,9 +57,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
